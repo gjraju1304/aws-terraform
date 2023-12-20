46d3dc5e7401eebc5435797d2a25573a6c0dbb59
gjraju1304 <gjraju1304@gmail.com>
2023-12-20 22:03:42 +0530

    used variables

---
+++ inputs.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  type = string
+  #   default = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "web_subnet1" {
+  type = string
+}
+
+variable "data_subnet2" {
+  type = string
+}
+
+variable "business_subnet3" {
+  type = string
+}
--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
   default_tags {
     tags = {
       Env     = "dev"
